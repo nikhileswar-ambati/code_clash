163a19572fbeb35f53c3a9713372969177193db4
nikhileswar-ambati <nikhileswarambati.007@gmail.com>
2026-07-22 00:23:34 +0530

    fix

--- Server/server/settings.py
+++ Server/server/settings.py
@@ -143,7 +143,12 @@ ALLOWED_HOSTS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    ...
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 
